3a82ef052438e4833affd95df98057d6c4830a61
Lucas Rodrigues Correia <lucas_rc15@live.com>
2023-08-20 16:59:22 -0300

    2.3 finalizado

--- core/crud.ts
+++ core/crud.ts
@@ -1,54 +1,87 @@
 import fs from "fs"; // ES6
-
-// const fs = require ("fs");
+import { v4 as uuid } from 'uuid';
+// const fs = require("fs"); - CommonJS
 const DB_FILE_PATH = "./core/db";
 
-console.log ("[CRUD]");
+console.log("[CRUD]");
 
 interface Todo {
-    date: string;
-    content:string;
-    done: boolean;
+  id: string;
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-function create (content:string) {
-    const todo = {
-        date: new Date().toISOString(),
-        content:content,
-        done:false,
-    };
+function create(content: string): Todo {
+  const todo: Todo = {
+    id: uuid(),
+    date: new Date().toISOString(),
+    content: content,
+    done: false,
+  };
 
-    const todos: Array<Todo> = [
-        ...read (),
-        todo,
-    ];
+  const todos: Array<Todo> = [
+    ...read(),
+    todo,
+  ];
 
-    // salvar o content no sistema
-    fs.writeFileSync (DB_FILE_PATH, JSON.stringify({
-        todos,
-        dogs: [],
-    }, null, 2));
-    return content;
-
+  // salvar o content no sistema
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+    dogs: [],
+  }, null, 2));
+  return todo;
 }
 
-function read() :Array<Todo> {
-    const dbString = fs.readFileSync (DB_FILE_PATH, "utf-8");
-    const db = JSON.parse(dbString || "{}");
-    if(!db.todos) { // FAIL FAST VALIDATIONS
-        return [];
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
+  if(!db.todos) { // Fail Fast Validations
+    return [];
+  }
+
+  return db.todos;
+}
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id === id;
+    if(isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
     }
-    return db.todos;
+  });
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos,
+  }, null, 2));
+
+  if(!updatedTodo) {
+    throw new Error("Please, provide another ID!")
+  }
+
+  return updatedTodo;
+}
+
+function updateContentById(id: string, content: string): Todo {
+  return update(id, {
+    content,
+  })
 }
 
 function CLEAR_DB() {
-    fs.writeFileSync(DB_FILE_PATH, "");
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
-// Simulaçã
-CLEAR_DB()
+// [SIMULATION]
+CLEAR_DB();
 create("Primeira TODO");
-create("Segunda TODO");
-
+create("Primeira TODO");
+const terceiraTodo = create("Segunda TODO");
+// update(terceiraTodo.id, {
+//   content: "Atualizada!",
+//   done: true,
+// });
+updateContentById(terceiraTodo.id, "Atualizada!")
 console.log(read());
-
